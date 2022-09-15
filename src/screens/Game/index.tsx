24b722b88e28691ae6ede8f5985771487c928007
Matheus Barboza <matheus.barboza102@gmail.com>
2022-09-15 20:52:12 -0300

    feat: tela game

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,18 +1,56 @@
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Background } from "../../components/Background";
 
+import { Entypo } from '@expo/vector-icons';
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { Image, TouchableOpacity, View } from 'react-native';
 import { GameParams } from "../../@types/navigation";
+import { Heading } from '../../components/Heading';
+import { THEME } from '../../theme';
 import { styles } from "./styles";
 
 export function Game() {
   const route = useRoute();
   const game = route.params as GameParams;
 
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
-        
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo 
+              name='chevron-thin-left'
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image 
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right}/>
+        </View>
+
+        <Image 
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode='cover'
+        />
+
+        <Heading 
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
       </SafeAreaView>
     </Background>
   );
--- src/screens/Game/styles.ts
+++ src/screens/Game/styles.ts
@@ -2,6 +2,28 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: 'space-between',
+  },
+  logo: {
+
+  },
+  right: {
+    width: 20,
+    height: 20,
+  },
+  cover: {
+    width: 311,
+    height: 160,
+    borderRadius: 8,
+    marginTop: 32,
   }
 });
